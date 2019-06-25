11946425b03b0f9471c92fe674620c011fdd61c2
Tominous <18189490+Tominous@users.noreply.github.com>
2019-06-25 09:53:29 +0100

    Update ForgeWorldEdit.java

--- worldedit-forge/src/main/java/com/sk89q/worldedit/forge/ForgeWorldEdit.java
+++ worldedit-forge/src/main/java/com/sk89q/worldedit/forge/ForgeWorldEdit.java
@@ -135,10 +135,10 @@ public class ForgeWorldEdit {
         WorldEdit.getInstance().getPlatformManager().register(platform);
 
 //  TODO      if (ModList.get().isLoaded("sponge")) {
-//            this.provider = new ForgePermissionsProvider.SpongePermissionsProvider();
-//        } else {
+            this.provider = new ForgePermissionsProvider.SpongePermissionsProvider();
+        } else {
         this.provider = new ForgePermissionsProvider.VanillaPermissionsProvider(platform);
-//        }
+        }
     }
 
     private void setupRegistries() {
